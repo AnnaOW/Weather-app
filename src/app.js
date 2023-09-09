b131b4897ef343748aa1e1eb6968ca74fc1f8b84
AnnaOW <anna.wierzbicka1996@gmail.com>
2023-09-09 19:07:08 +0200

    Fixed styling of the next days forecast

--- src/app.js
+++ src/app.js
@@ -56,15 +56,17 @@ function displayForecast() {
       forecastHTML +
       `
       <div class="col-2">
+      <div class = "next-days-forecast-card">
         <div class="weather-forecast-date">${day}</div>
-        <img
+        <img class = "next-day-weather-picture"
           src="http://openweathermap.org/img/wn/50d@2x.png"
           alt=""
-          width="42"
+          width="45"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> 18° | </span>
           <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
         </div>
       </div>
   `;
